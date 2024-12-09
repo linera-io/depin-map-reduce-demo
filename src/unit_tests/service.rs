36fe78253f75ccac0c04243ad31be25ed0fb9be4
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2024-12-09 18:00:06 +0000

    Simplify the service unit test
    Use the newly added helpers.

--- src/unit_tests/service.rs
+++ src/unit_tests/service.rs
@@ -4,7 +4,6 @@
 use std::sync::Arc;
 
 use async_graphql::{Request, Response, Value};
-use futures::FutureExt as _;
 use linera_sdk::{util::BlockingWait, views::View, Service, ServiceRuntime};
 use serde_json::json;
 
@@ -14,22 +13,12 @@ use super::{DepinDemoService, DepinDemoState};
 #[test]
 fn value_query() {
     let value = 60u64;
-    let runtime = ServiceRuntime::<DepinDemoService>::new();
-    let mut state = DepinDemoState::load(runtime.root_view_storage_context())
-        .blocking_wait()
-        .expect("Failed to read from mock key value store");
-    state.value.set(value);
+    let mut service = create_service();
 
-    let service = DepinDemoService {
-        state: Arc::new(state),
-        runtime,
-    };
+    service.state.edit().value.set(value);
+
     let request = Request::new("{ value }");
-
-    let response = service
-        .handle_query(request)
-        .now_or_never()
-        .expect("Query should not await anything");
+    let response = service.handle_query(request).blocking_wait();
 
     let expected = Response::new(Value::from_json(json!({"value": 60})).unwrap());
 
