7800c2dbea8e411c46b783e336f2eef55c193600
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2024-12-09 17:49:30 +0000

    Add a `DepinDemoState::edit` test helper method
    Prepare to allow editing a test service instance's state.

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,9 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+#[cfg(test)]
+use std::sync::Arc;
+
 use linera_sdk::{
     base::ChainId,
     views::{linera_views, RegisterView, RootView, ViewStorageContext},
@@ -11,4 +14,13 @@ use linera_sdk::{
 pub struct DepinDemoState {
     pub parent: RegisterView<Option<ChainId>>,
     pub value: RegisterView<u64>,
+}
+
+#[cfg(test)]
+#[allow(dead_code)]
+impl DepinDemoState {
+    /// Allows editing the state in a service implementation in tests.
+    pub fn edit(self: &mut Arc<Self>) -> &mut Self {
+        Arc::get_mut(self).expect("State can not be edited when it is being shared")
+    }
 }
